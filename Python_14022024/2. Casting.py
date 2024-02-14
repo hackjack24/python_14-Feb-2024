c9fab18204826e26a530ae06b56c8cecd712002b
hackjack <hackjack2413@gmail.com>
2024-02-14 17:23:29 +0800

    Python.2_Casting

---
+++ Python_14022024/2. Casting.py
@@ -0,0 +1,47 @@
+# Integer
+print("=====INTEGER=====")
+data_int = 9
+print("data :", data_int, ",type =", type(data_int))
+
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int)
+print("data :", data_float, ",type =", type(data_float))
+print("data :", data_str, ",type =", type(data_str))
+print("data :", data_bool, ",type =", type(data_bool))
+
+# FLOAT
+print("=====FLOAT=====")
+data_float = 20.5
+print("data :", data_float, ",type =", type(data_float))
+
+data_int = int(data_float)
+data_str = str(data_float)
+data_bool = bool(data_float)
+print("data :", data_int, ",type =", type(data_int))
+print("data :", data_str, ",type =", type(data_str))
+print("data :", data_bool, ",type =", type(data_bool))
+
+# BOOLEAN
+print("=====BOOLEAN=====")
+data_bool = True
+print("data :", data_bool, ",type =", type(data_bool))
+
+data_int = int(data_bool)
+data_str = str(data_bool)
+data_float = float(data_bool)
+print("data :", data_int, ",type =", type(data_int))
+print("data :", data_str, ",type =", type(data_str))
+print("data :", data_float, ",type =", type(data_float))
+
+# STRING
+print("=====STRING=====")
+data_str = "HACKJACK"
+print("data :", data_str, ",type =", type(data_str))
+
+data_int = int(data_str)
+data_float = float(data_str)
+data_bool = bool(data_str)
+print("data :", data_int, ",type =", type(data_int))
+print("data :", data_float, ",type =", type(data_float))
+print("data :", data_bool, ",type =", type(data_bool))
