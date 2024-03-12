245cabbab46d634c5d87459c6f88e7db80b4a0bc
hackjack <hackjack2413@gmail.com>
2024-03-12 14:39:54 +0800

    8.assignment

---
+++ 8.operatorAssignment.py
@@ -0,0 +1,14 @@
+# operasi Assignment
+
+a = 5
+
+a += 3 #artinya adalah a = a + 3 (disingkat)
+print("nilai a=",a)
+print("=====================================")
+
+a -= 3 #artinya adalah a = a - 3 (disingkat)
+print("nilai a=",a)
+print("=====================================")
+
+a *= 3 #artinya adalah a = a * 3 (disingkat)
+print("nilai a=",a)
